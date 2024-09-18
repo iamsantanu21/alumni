2a4e8d923301a41239463ec0485b36676958213c
Santanu Mondal <iamsantanu21@gmail.com>
2024-09-18 19:34:24 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,30 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+    ,<h2>About the Department</h2>
+    <p>The Department aims at imparting quality education in Computer Science & Engineering and Information Technology through various post-graduate programmes. It also offers an atmosphere conducive for research scholars for pursuing research in various advanced areas of Computer Science, Engineering and Information Technology. The Computer Science students’ association, PIXEL, provides opportunities to sharpen the skills of students by organizing lectures and students meet. The alumni of the department deliver lectures on latest topics periodically for updating the skill sets of the students.</p>
+    <h2>Programmes Offered</h2> 
+    <ul>
+        <li>B.Tech. Bachelor of Technology</li>
+        <li>M.C.A. Master of Computer Applications</li>
+        <li>M.Sc. Computer Science</li>
+        <li>M.Sc. Computer Science (5 Years Integrated Programme)</li>
+        <li>M.Tech. Computer Science & Engineering</li>
+        <li>M.Tech. Network & Information Security</li>
+        <li>Ph.D. Computer Science & Engineering</li>
+    </ul>       
+            
+        </div>
+        </section>
